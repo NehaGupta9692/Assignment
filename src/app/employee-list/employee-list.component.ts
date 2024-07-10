f05417a0df0e8ce308c2e720c60cc372727610b0
Neha Kumari <nehakumari.jsr234@gmail.com>
2024-07-10 10:42:18 +0530

    new chnages

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,28 +1,33 @@
-import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
-  imports: [CommonModule,HttpClientModule,FormsModule],
+  imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './employee-list.component.html',
-  styleUrl: './employee-list.component.css'
+  styleUrl: './employee-list.component.css',
 })
 export class EmployeeListComponent {
   apiUrl = 'http://localhost:3000/items';
   users: any[] = [];
-  newUser: any = { firstname: '',lastname:'',phoneno:'', address: '', email:'' };
+  newUser: any = {
+    firstname: '',
+    lastname: '',
+    phoneno: '',
+    address: '',
+    email: '',
+  };
   editing = false;
-  constructor(private http:HttpClient,private route:Router){}
+  constructor(private http: HttpClient, private route: Router) {}
   ngOnInit(): void {
     this.fetchUsers();
   }
   fetchUsers() {
-    this.http.get<any[]>(this.apiUrl).subscribe(users => {
+    this.http.get<any[]>(this.apiUrl).subscribe((users) => {
       this.users = users;
       this.newUser = this.users.map(() => false);
     });
@@ -33,14 +38,15 @@ export class EmployeeListComponent {
   //     this.fetchUsers();
   //   });
   // }
-  
+
   toggleEdit(index: number) {
-    this.newUser[index] = true
+    this.newUser[index] = true;
   }
 
   saveUser(index: number) {
-    this.http.put(`${this.apiUrl}/${this.users[index].id}`, this.users[index])
-      .subscribe(response => {
+    this.http
+      .put(`${this.apiUrl}/${this.users[index].id}`, this.users[index])
+      .subscribe((response) => {
         this.newUser[index] = false;
         console.log(response);
       });
